a4131bf590c4de3c6c8fd16fb2a3c76740dd882b
Bommi venkata sai <bommisai23@gmail.com>
2026-07-22 21:53:04 +0530

    sports added

--- frontend/src/components/ReviewCard.jsx
+++ frontend/src/components/ReviewCard.jsx
@@ -25,7 +25,7 @@ const EXPANDABLE_SECTIONS = [
   ['faculty', 'Faculty'],
   ['placements', 'Placements'],
   ['tech_events', 'Events'],
-  ['infrastructure', 'Infrastructure and Sports'],
+  ['infrastructure', 'Infrastructure & Sports'],
   ['college_life', 'College life'],
   ['accommodation', 'Hostel life'],
 ];
